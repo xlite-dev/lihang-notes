cdcad3b7aa944ddd3b1f4b4055ced54d6722953f
DefTruth <31974251+DefTruth@users.noreply.github.com>
2025-05-17 09:50:47 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,1 +1,1 @@
-wget https://github.com/DefTruth/statistic-learning-R-note/releases/download/v0.1.0/statistic.learning.R.Note.v0.1.0.pdf
+wget https://github.com/xlite-dev/lihang-notes/releases/download/v0.1.0/statistic.learning.R.Note.v0.1.0.pdf
